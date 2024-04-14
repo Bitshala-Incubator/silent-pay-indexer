142add8565b500972986933b7e727f93d2232452
Anmol Sharma <anmolsharma0234@gmail.com>
2024-04-14 12:51:08 +0530

    feat: connect to postgress on startup
    Signed-off-by: Anmol Sharma <anmolsharma0234@gmail.com>

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from '@/app.controller';
 import { AppService } from '@/app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-    imports: [],
+    imports: [
+        TypeOrmModule.forRoot({
+            type: 'postgres',
+            host: 'localhost',
+            port: 5432,
+            username: 'root',
+            password: 'password',
+            database: 'silent-pay-indexer',
+            synchronize: true,
+            autoLoadEntities: true,
+        }),
+    ],
     controllers: [AppController],
     providers: [AppService],
 })
