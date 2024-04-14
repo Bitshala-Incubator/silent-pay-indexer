6eff7644de981541563be92c4670a87b3a81e631
Anmol Sharma <anmolsharma0234@gmail.com>
2024-04-14 23:10:46 +0530

    feat: add index transaction command and template for it's handler
    Signed-off-by: Anmol Sharma <anmolsharma0234@gmail.com>

---
+++ src/commands/index-transaction.command.ts
@@ -0,0 +1,22 @@
+export type TransactionInput = {
+    txid: string; // transaction id
+    vout: number; // output index
+    scriptSig: string; // unlocking script
+    witness?: string[]; // witness data
+    prevOutScript: string; // previous output script
+};
+
+export type TransactionOutput = {
+    scriptPubKey: string;
+    value: number;
+};
+
+export class IndexTransactionCommand {
+    constructor(
+        public readonly txid: string,
+        public readonly vin: TransactionInput[],
+        public readonly vout: TransactionOutput[],
+        private readonly blockHeight: number,
+        private readonly blockHash: string,
+    ) {}
+}
---
+++ src/handlers/commands/index-transaction.handler.ts
@@ -0,0 +1,11 @@
+import { CommandHandler, ICommandHandler } from '@nestjs/cqrs';
+import { IndexTransactionCommand } from '@/commands/index-transaction.command';
+
+@CommandHandler(IndexTransactionCommand)
+export class IndexTransactionHandler
+    implements ICommandHandler<IndexTransactionCommand>
+{
+    async execute(command: IndexTransactionCommand) {
+        throw new Error(`Method not implemented. Command: ${command}`);
+    }
+}
