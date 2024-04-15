eeae14c676b8caad988b62816d5beaf1556d69dc
ayman <yaoiotakusama@gmail.com>
2024-04-15 09:30:18 +0530

    feat: add db config validation
    feat: add additional config validation
    
    chore: add dependencies

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,8 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import configuration from '@/configuration';
 import { TransactionsModule } from '@/transactions/transactions.module';
+import { validate } from 'class-validator';
+import { DbConfig } from './dbConfiguration';
 
 @Module({
     imports: [
@@ -16,16 +18,22 @@ import { TransactionsModule } from '@/transactions/transactions.module';
         TypeOrmModule.forRootAsync({
             imports: [ConfigModule],
             inject: [ConfigService],
-            useFactory: (configService: ConfigService) => ({
-                type: 'postgres',
-                host: configService.get<string>('db.host'),
-                port: configService.get<number>('db.port'),
-                username: configService.get<string>('db.username'),
-                password: configService.get<string>('db.password'),
-                database: configService.get<string>('db.databaseName'),
-                synchronize: configService.get<boolean>('db.synchronize'),
-                autoLoadEntities: true,
-            }),
+            useFactory: (configService: ConfigService) => {
+                const dbConfig = new DbConfig(configService);
+                validate(dbConfig).then((errors) => {
+                    for (const error of errors) {
+                        console.error(error.toString());
+                    }
+
+                    throw new Error('Invalid config');
+                });
+
+                return {
+                    type: 'postgres',
+                    ...dbConfig,
+                    autoLoadEntities: true,
+                };
+            },
         }),
         TransactionsModule,
     ],
