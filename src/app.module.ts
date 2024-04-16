abd8b210154a253d767a87d64bce3a50b689ad55
ayman <yaoiotakusama@gmail.com>
2024-04-16 15:53:21 +0530

    chore: revert to old config logic

--- src/app.module.ts
+++ src/app.module.ts
@@ -17,18 +17,16 @@ import { validate } from 'class-validator';
         TypeOrmModule.forRootAsync({
             imports: [ConfigModule],
             inject: [ConfigService],
-            useFactory: (configService: ConfigService) => {
-                return {
-                    type: 'postgres',
-                    host: configService.get('db.host'),
-                    port: configService.get('db.port'),
-                    username: configService.get('db.username'),
-                    password: configService.get('db.password'),
-                    databaseName: configService.get('db.databaseName'),
-                    synchronize: configService.get('db.synchronize'),
-                    autoLoadEntities: true,
-                };
-            },
+            useFactory: (configService: ConfigService) => ({
+                type: 'postgres',
+                host: configService.get<string>('db.host'),
+                port: configService.get<number>('db.port'),
+                username: configService.get<string>('db.username'),
+                password: configService.get<string>('db.password'),
+                database: configService.get<string>('db.databaseName'),
+                synchronize: configService.get<boolean>('db.synchronize'),
+                autoLoadEntities: true,
+            }),
         }),
         TransactionsModule,
     ],
