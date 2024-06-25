b42d83d70836cbfc9419eda36a59b7a9c937d9ee
Anmol Sharma <anmolsharma0234@gmail.com>
2024-06-25 12:56:05 +0530

    feat: add config for esplora provider
    Signed-off-by: Anmol Sharma <anmolsharma0234@gmail.com>

--- src/common/enum.ts
+++ src/common/enum.ts
@@ -1,3 +1,14 @@
 export enum ServiceStatus {
     HEALTHY = 'HEALTHY',
 }
+
+export enum ProviderType {
+    ESPLORA = 'ESPLORA',
+    BITCOIN_CORE_RPC = 'BITCOIN_CORE_RPC',
+}
+
+export enum BitcoinNetwork {
+    MAINNET = 'main',
+    TESTNET = 'testnet',
+    REGTEST = 'regtest',
+}
--- src/configuration.model.ts
+++ src/configuration.model.ts
@@ -1,14 +1,18 @@
 import {
     IsBoolean,
     IsDefined,
+    IsEnum,
     IsInt,
     IsNotEmpty,
     IsString,
+    IsUrl,
     Max,
     Min,
+    ValidateIf,
     ValidateNested,
 } from 'class-validator';
 import { Type } from 'class-transformer';
+import { BitcoinNetwork, ProviderType } from '@/common/enum';
 
 class DbConfig {
     @IsNotEmpty()
@@ -41,6 +45,18 @@ class AppConfig {
     @Min(1)
     @Max(65535)
     port: number;
+
+    @IsEnum(BitcoinNetwork)
+    network: BitcoinNetwork;
+}
+
+class EsploraConfig {
+    @IsUrl({
+        protocols: ['http', 'https'],
+        require_protocol: true,
+        require_host: true,
+    })
+    url: string;
 }
 
 export class Config {
@@ -53,4 +69,13 @@ export class Config {
     @ValidateNested()
     @Type(() => AppConfig)
     app: AppConfig;
+
+    @IsEnum(ProviderType)
+    providerType: ProviderType;
+
+    @ValidateIf((o) => o.providerType === ProviderType.ESPLORA)
+    @IsDefined()
+    @ValidateNested()
+    @Type(() => EsploraConfig)
+    esplora: EsploraConfig;
 }
