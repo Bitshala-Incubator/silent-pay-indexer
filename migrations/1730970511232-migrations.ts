8a7a801985516390a882159f37730ea763db5955
Anmol Sharma <anmolsharma0234@gmail.com>
2025-05-04 17:15:44 +0530

    feat: add new table to store transaction output
    Signed-off-by: Anmol Sharma <anmolsharma0234@gmail.com>

--- migrations/1730970511232-migrations.ts
+++ migrations/1730970511232-migrations.ts
@@ -5,7 +5,7 @@ export class Migrations1730970511232 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(
-            `CREATE TABLE "transaction" ("id" text PRIMARY KEY NOT NULL, "blockHeight" integer NOT NULL, "blockHash" text NOT NULL, "scanTweak" text NOT NULL, "outputs" text NOT NULL, "isSpent" boolean NOT NULL)`,
+            `CREATE TABLE "transaction" ("id" text PRIMARY KEY NOT NULL, "blockHeight" integer NOT NULL, "blockHash" text NOT NULL, "scanTweak" text NOT NULL)`,
         );
         await queryRunner.query(
             `CREATE TABLE "block_state" ("blockHeight" integer PRIMARY KEY NOT NULL, "blockHash" text NOT NULL)`,
@@ -13,9 +13,13 @@ export class Migrations1730970511232 implements MigrationInterface {
         await queryRunner.query(
             `CREATE TABLE "operation_state" ("id" text PRIMARY KEY NOT NULL, "state" text NOT NULL)`,
         );
+        await queryRunner.query(
+            `CREATE TABLE "transaction_output" ("transactionId" varchar(64) NOT NULL, "vout" integer NOT NULL, "pubKey" varchar(64) NOT NULL, "value" integer NOT NULL, "isSpent" boolean NOT NULL DEFAULT (0), CONSTRAINT "FK_d349c84cef0c11debd68e6e3365" FOREIGN KEY ("transactionId") REFERENCES "transaction" ("id") ON DELETE CASCADE ON UPDATE NO ACTION, PRIMARY KEY ("transactionId", "vout"))`,
+        );
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`DROP TABLE "transaction_output"`);
         await queryRunner.query(`DROP TABLE "operation_state"`);
         await queryRunner.query(`DROP TABLE "block_state"`);
         await queryRunner.query(`DROP TABLE "transaction"`);
